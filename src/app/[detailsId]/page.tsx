be2b2f3c9ffd7c92e7aaae965a9c060809f643e8
alialaba <alialaba079@gmail.com>
2025-01-30 12:48:59 -0500

    feat: implement nftcard with show more and less ft

--- src/app/[detailsId]/page.tsx
+++ src/app/[detailsId]/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "@/styles/details.css";
+import NFTGrid from "@/components/detail/NftGrid";
 
 const Detail = () => {
-  return <section className="nft-details">Detail</section>;
+  return <section className="nft-details ">
+    <h1 className="bg-red">Hetllooe</h1>
+     <NFTGrid/>
+    </section>;
 };
 
 export default Detail;
