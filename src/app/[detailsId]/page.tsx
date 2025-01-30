1553776921885c49b78ae97094561dca9780fa37
alialaba <alialaba079@gmail.com>
2025-01-30 13:02:59 -0500

    feat: implement nft product card

--- src/app/[detailsId]/page.tsx
+++ src/app/[detailsId]/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "@/styles/details.css";
 import NFTGrid from "@/components/detail/NftGrid";
+import Product from "@/components/detail/Product";
 
 const Detail = () => {
   return <section className="nft-details ">
     <h1 className="bg-red">Hetllooe</h1>
      <NFTGrid/>
+     <Product/>
     </section>;
 };
 
