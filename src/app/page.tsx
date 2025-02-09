6b5f6a60e511e0df5e14aab5931c68aec462db0c
julietoge <julietogechi27@gmail.com>
2025-02-09 17:37:18 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import LiveAuction from '@/components/listingPage/liveAuction';
-import Logotypes from '@/components/listingPage/logotypes';
+// import Logotypes from '@/components/listingPage/logotypes';
 import HeroSection from '@/components/listingPage/heroSection';
+
 
 const ListingPage = () => {
   return (
     <div className="listing-page">
       <HeroSection/>
       {/* logo types brands */}
-      <Logotypes/>
+      {/* <Logotypes/> */}
       {/* Live Auction */}
       <LiveAuction/>
     </div>
--- src/components/listingPage/heroSection.tsx
+++ src/components/listingPage/heroSection.tsx
@@ -1,38 +1,39 @@
 import React from "react";
-// import nftText from "../../assets/Text.png";
+import nftText from "../../assets/Text.png";
+import Image from "next/image";
+import Logotypes from "./logotypes";
 
 const HeroSection = () => {
   return (
-    <div className="h-screen bg-hero-pattern flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">Welcome to NTF Listings</h1>
+    <div className="flex flex-col items-center justify-center bg-[url('/Background.png')] bg-cover bg-center relative">
+      <div className="absolute bg-[url('/Squares.png')] bg-contain bottom-0 inset-x-0 z-[-2] h-[655px] pointer-events-none"></div>
+      <div className="mt-48">
+        <Image
+          src={nftText}
+          width={837}
+          height={351}
+          priority={false}
+          alt="nftTextImg"
+        />
+      </div>
+      <div className="flex justify-center items-center w-[643px] h-[84px] bg-rest-white rounded-[16px] shadow-[0px_25px_75px_0px_rgba(6,7,20,0.1)] mt-20">
+        <input
+          type="search"
+          className="flex-grow h-full rounded-[16px] font-custom-16 focus:outline-none px-2 text-On-White-Dark-90 font-inter leading-[24px] tracking-[0.2px]"
+          placeholder="Items, collections and creators"
+          aria-label="Search"
+        />
+        <select
+          className="flex-grow h-full rounded-[16px] font-custom-16 focus:outline-none px-2 text-On-White-Dark-90 font-inter leading-[24px] tracking-[0.2px]"
+          aria-label="Filter options"
+        >
+          <option value="">Categories</option>
+          <option value="products">Products</option>
+          <option value="brands">Brands</option>
+        </select>
+      </div>
+      <Logotypes />
     </div>
-    //     <section className="nft-header-section">
-
-//       <div className="nftText">
-//         <Image
-//           src={nftText}
-//           width={837}
-//           height={351}
-//           priority={false}
-//           alt="nftTextImg"
-//         />
-//       </div>
-//       <div className="search-select">
-//         <div className="search-select-content">
-//           <input
-//             type="search"
-//             className="search"
-//             placeholder="Items, collections and creators"
-//             aria-label="Search"
-//           />
-//           <select className="" aria-label="Filter options">
-//             <option value="">Categories</option>
-//             <option value="products">Products</option>
-//             <option value="brands">Brands</option>
-//           </select>
-//         </div>
-//       </div>
-//     </section>
   );
 };
 
